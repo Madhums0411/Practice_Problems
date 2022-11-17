f3eb9786c0ac896cf760a15401e37c3b96333eb4
madhums0411 <madhums41175@gmail.com>
2022-11-17 22:42:20 +0530

    Aggregation practice problem

---
+++ PracticeProblems/PracticeProblems/Aggregation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class Address
+    {
+        public string addressLine, city, state;
+        public Address(string addressLine, string city, string state)
+        {
+            this.addressLine = addressLine;
+            this.city = city;
+            this.state = state;
+        }
+    }
+    public class Employe
+    {
+        public int id;
+        public string name;
+        public Address address; 
+        public Employe(int id, string name, Address address)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " +
+              address.addressLine + " " + address.city + " " + address.state);
+        }
+    }
+
+}
--- PracticeProblems/PracticeProblems/Program.cs
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -1,9 +1,17 @@
 ﻿//// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 using PracticeProblems;
-{
-    Programmer p1 = new Programmer();
+using System.Net;
+using System.Xml.Linq;
 
-    Console.WriteLine("Salary: " + p1.salary);
-    Console.WriteLine("Bonus: " + p1.bonus);
-}
+
+    ////Inheritance
+    //Programmer p1 = new Programmer();
+
+    //Console.WriteLine("Salary: " + p1.salary);
+    //Console.WriteLine("Bonus: " + p1.bonus);
+
+    //Aggregation 
+    Address a1 = new Address("G-13, Sec-3", "Noida", "UP");
+    Employe e1 = new Employe(1, "Sonoo", a1);
+    e1.display();
