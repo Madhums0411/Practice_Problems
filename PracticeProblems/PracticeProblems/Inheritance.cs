6c1cabe4a2008fd477ca10bc801d375e13fc7b9b
madhums0411 <madhums41175@gmail.com>
2022-11-17 22:06:32 +0530

    Inheritance practice problem

---
+++ PracticeProblems/PracticeProblems/Inheritance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+}
---
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -0,0 +1,9 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using PracticeProblems;
+{
+    Programmer p1 = new Programmer();
+
+    Console.WriteLine("Salary: " + p1.salary);
+    Console.WriteLine("Bonus: " + p1.bonus);
+}
